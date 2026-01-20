88ba6b05a94ed85746bac9f617f8cef95cf1ff20
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-01-20 23:25:13 +0300

    Demonstrate equals, copy and toString for Task

--- src/Task.kt
+++ src/Task.kt
@@ -8,3 +8,13 @@ data class Task(
     var  isCompleted : Boolean = false,
 
 ){}
+fun main(){
+    val task1 = Task("Carry papers", "Carry papers for Boss", Priority.high, true)
+    val task2 = Task("Carry papers", "Carry papers for Boss", Priority.high, true)
+    val task3 = Task("Make a cup of coffee", "Make a cup of for yourself", Priority.low,)
+    println(task1)
+    println("${task1 == task2}")
+    println("${task2.hashCode()}")
+    val task4 = task1.copy(priority = Priority.medium)
+    println(task4)
+}
