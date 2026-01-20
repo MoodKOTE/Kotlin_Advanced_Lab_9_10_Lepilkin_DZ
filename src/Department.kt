78984e256c51549e0730f10ee586aed8c9ca2c61
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-01-20 23:46:02 +0300

    ImplementDevelopmentDepartment and TestingDepartment

--- src/Department.kt
+++ src/Department.kt
@@ -1,2 +1,28 @@
 abstract class Department {
+    abstract var departmentName: String
+    abstract fun  printDepartmentGoal()
+}
+
+class DevelopmentDepartment : Department(){
+    override var departmentName: String = "Разработка"
+    override fun printDepartmentGoal() {
+        println("Пишет чистый код")
+    }
+
+}
+class TestingDepartment : Department(){
+    override var departmentName: String = "Тестирование"
+    override fun printDepartmentGoal() {
+        println("Находит все баги")
+    }
+}
+fun main() {
+    val devDepartment = DevelopmentDepartment()
+    val testDepartment = TestingDepartment()
+
+    println("${devDepartment.departmentName}:")
+    devDepartment.printDepartmentGoal()
+
+    println("${testDepartment.departmentName}:")
+    testDepartment.printDepartmentGoal()
 }
